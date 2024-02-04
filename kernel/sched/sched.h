18fa52341a0a1eb88e99ade65b320bf3f9796d3a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-02-04 16:20:29 +0800

    sched: cleanup!
    walt only things arent needed, we going pelt+cass

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2708,7 +2708,11 @@ static inline void restore_cgroup_boost_settings(void) { }
 
 extern int alloc_related_thread_groups(void);
 
+extern unsigned long all_cluster_ids[];
+
+#ifdef CONFIG_SCHED_WALT
 extern void check_for_migration(struct rq *rq, struct task_struct *p);
+#endif /* bye walt */
 
 static inline int is_reserved(int cpu)
 {
@@ -2813,7 +2817,9 @@ static inline bool task_placement_boost_enabled(struct task_struct *p)
 	return false;
 }
 
+#ifdef CONFIG_SCHED_WALT
 static inline void check_for_migration(struct rq *rq, struct task_struct *p) { }
+#endif /* bye walt */
 
 static inline int sched_boost(void)
 {
