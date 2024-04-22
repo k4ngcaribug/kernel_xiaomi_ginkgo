597dd9c85c02aa559459904a3ff370d87cb1d9ef
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-04-22 12:08:00 +0800

    ARM64/config: ginkgo: enable KernelSU + SuSFS

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Copyright (C) 2023 Edwiin Kusuma Jaya (ryuzenn)
-# Copyright (C) 2025 k4ngcaribug
+#
 # Simple Local Kernel Build Script
 #
 # Configured for Redmi Note 8 / ginkgo custom kernel source
@@ -12,7 +12,6 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="neophyte-v1.0-Ginkgo-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
-ZIPNAME_KSU="neophyte-v1.0-Ginkgo-KSU-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
 TC_DIR="$(pwd)/../tc/"
 CLANG_DIR="${TC_DIR}clang"
 GCC_64_DIR="${TC_DIR}aarch64-linux-android-4.9"
@@ -27,8 +26,9 @@ export LOCALVERSION
 
 if ! [ -d "${CLANG_DIR}" ]; then
 echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 -b 17 https://gitlab.com/nekoprjkt/aosp-clang ${CLANG_DIR}; then
+if ! git clone --depth=1 https://gitlab.com/nekoprjkt/aosp-clang ${CLANG_DIR}; then
 echo "Cloning failed! Aborting..."
+exit 1
 fi
 fi
 
@@ -36,6 +36,7 @@ if ! [ -d "${GCC_64_DIR}" ]; then
 echo "gcc not found! Cloning to ${GCC_64_DIR}..."
 if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
 echo "Cloning failed! Aborting..."
+exit 1
 fi
 fi
 
@@ -43,24 +44,8 @@ if ! [ -d "${GCC_32_DIR}" ]; then
 echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
 if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
 echo "Cloning failed! Aborting..."
+exit 1
 fi
-fi
-
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-	echo -e "\nCleanup KernelSU first on local build\n"
-	rm -rf KernelSU drivers/kernelsu
-else
-	echo -e "\nSet No KernelSU Install, just skip\n"
-fi
-
-# Set function for override kernel name and variants
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-echo -e "\nKSU Support, let's Make it On\n"
-curl -LSs "https://raw.githubusercontent.com/KernelSu-Next/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
-git apply KernelSU-hook.patch
-sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' arch/arm64/configs/vendor/ginkgo_defconfig
-else
-echo -e "\nKSU not Support, let's Skip\n"
 fi
 
 mkdir -p out
@@ -81,37 +66,28 @@ make -j$(nproc --all) O=out \
 					  CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
 					  CLANG_TRIPLE=aarch64-linux-gnu- \
 					  Image.gz-dtb \
-					  dtbo.img | tee error.log
+					  dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
-git restore arch/arm64/configs/vendor/ginkgo_defconfig
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
 elif ! git clone -q https://github.com/k4ngcaribug/AnyKernel3; then
 echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+exit 1
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
 git checkout main &> /dev/null
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-zip -r9 "../$ZIPNAME_KSU" * -x '*.git*' README.md *placeholder
-else
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-fi
 cd ..
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
 echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-echo "Zip: $ZIPNAME_KSU"
-else
 echo "Zip: $ZIPNAME"
-fi
 else
 echo -e "\nCompilation failed!"
 fi
 echo -e "======================================="
-git restore .
