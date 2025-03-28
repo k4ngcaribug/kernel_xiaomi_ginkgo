6cbcc9d9c692b54de23157b7695add245559d6cf
k4ngcaribug <neophyteplayground@gmail.com>
2025-03-28 23:13:44 +0700

    [SQUASH] silence log spam

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -533,7 +533,7 @@ static int smb2_usb_set_prop(struct power_supply *psy,
 		rc = smblib_set_prop_sdp_current_max(chg, val);
 		break;
 	default:
-		pr_err("set prop %d is not supported\n", psp);
+		pr_debug("set prop %d is not supported\n", psp);
 		rc = -EINVAL;
 		break;
 	}
