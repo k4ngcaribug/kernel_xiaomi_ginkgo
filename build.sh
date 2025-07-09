6ad7d2bd0864be2803e0760ed84c7a4d8c399080
k4ngcaribug <neophyteplayground@gmail.com>
2025-07-09 16:16:31 +0000

    Kernel: Prepare v1.0 release

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Copyright (C) 2023 Edwiin Kusuma Jaya (ryuzenn)
-# Copyright (C) 2025 k4ngcaribug
+#
 # Simple Local Kernel Build Script
 #
 # Configured for Redmi Note 8 / ginkgo custom kernel source
@@ -11,9 +11,8 @@
 # Use this script on root of kernel directory
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="Neoohyte-Ginkgo-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
-ZIPNAME_KSU="Neophyte-Ginkgo-KSU-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
-TC_DIR="/workspace/gitpod/tc/"
+ZIPNAME="neophyte-v1.0-Ginkgo-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+TC_DIR="$(pwd)/../tc/"
 CLANG_DIR="${TC_DIR}clang"
 GCC_64_DIR="${TC_DIR}aarch64-linux-android-4.9"
 GCC_32_DIR="${TC_DIR}arm-linux-androideabi-4.9"
@@ -27,7 +26,7 @@ export LOCALVERSION
 
 if ! [ -d "${CLANG_DIR}" ]; then
 echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 -b 17 https://gitlab.com/nekoprjkt/aosp-clang ${CLANG_DIR}; then
+if ! git clone --depth=1 https://gitlab.com/nekoprjkt/aosp-clang ${CLANG_DIR}; then
 echo "Cloning failed! Aborting..."
 fi
 fi
@@ -44,23 +43,6 @@ echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
 if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
 echo "Cloning failed! Aborting..."
 fi
-fi
-
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-	echo -e "\nCleanup KernelSU first on local build\n"
-	rm -rf KernelSU drivers/kernelsu
-else
-	echo -e "\nSet No KernelSU Install, just skip\n"
-fi
-
-# Set function for override kernel name and variants
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-echo -e "\nKSU Support, let's Make it On\n"
-curl -LSs "https://raw.githubusercontent.com/KernelSu-Next/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
-git apply KernelSU-hook.patch
-sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' arch/arm64/configs/vendor/ginkgo_defconfig
-else
-echo -e "\nKSU not Support, let's Skip\n"
 fi
 
 mkdir -p out
@@ -81,11 +63,10 @@ make -j$(nproc --all) O=out \
 					  CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
 					  CLANG_TRIPLE=aarch64-linux-gnu- \
 					  Image.gz-dtb \
-					  dtbo.img | tee error.log
+					  dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
-git restore arch/arm64/configs/vendor/ginkgo_defconfig
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
 elif ! git clone -q https://github.com/k4ngcaribug/AnyKernel3; then
@@ -96,22 +77,13 @@ cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
 git checkout main &> /dev/null
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-zip -r9 "../$ZIPNAME_KSU" * -x '*.git*' README.md *placeholder
-else
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-fi
 cd ..
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
 echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-echo "Zip: $ZIPNAME_KSU"
-else
 echo "Zip: $ZIPNAME"
-fi
 else
 echo -e "\nCompilation failed!"
 fi
 echo -e "======================================="
-git restore .
